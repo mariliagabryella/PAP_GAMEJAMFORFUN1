eb4a8416acf148c72e3533cf3d2708b372e4319f
Letícia Rodrigues <12.leticia.rodrigues@gmail.com>
2025-11-25 10:49:50 +0000

    2.ºCommit

---
+++ edicao3(1).php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GameJamForFun - 3ª Edição</title>
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="style-edicao2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+
+<body>
+    <?php include 'menu.php'; ?>
+
+    <div class="conteudo"></div>
+
+    <section id="edicao2" class="edicao">
+        <div class="background"></div>
+        <div class="container">
+            <h1 class="titulo-principal">
+                Brevemente 
+                <br>
+                3ª Edição
+                <span class="edicao-numero">6,7 e 8 de Março de 2026</span>
+            </h1>
+</body>
+            </html>
