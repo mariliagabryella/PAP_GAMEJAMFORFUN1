9d2968f489de7cdf0d0865c94cebb5bab5eefc38
Letícia Rodrigues <12.leticia.rodrigues@gmail.com>
2025-11-26 15:12:45 +0000

    5.ºCommit

--- edicao3(1).php
+++ edicao3(1).php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameJamForFun - 3ª Edição</title>
     <link rel="icon" type="image/x-icon" href="img/logo.png">
-    <link rel="stylesheet" href="style3.css">
-    <link rel="stylesheet" href="style-edicao2.css">
+    <link rel="stylesheet" href="css/style3.css">
+    <link rel="stylesheet" href="css/style-edicao2.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
