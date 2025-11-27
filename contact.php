0c8a337d500923685e1e7700f58854a1ca6c0761
Letícia Rodrigues <12.leticia.rodrigues@gmail.com>
2025-11-27 17:14:56 +0000

    8.Commit

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Ajusta a visualização para dispositivos móveis -->
     <title>GameJamForFun</title>
     <link rel="icon" type="image/x-icon" href="img/logo.png">
-    <link rel="stylesheet" href="style4.css"> <!-- Importa o arquivo de estilos CSS -->
+    <link rel="stylesheet" href="css/style4.css"> <!-- Importa o arquivo de estilos CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="https://kit.fontawesome.com/YOUR-FONT-AWESOME-KIT.js" crossorigin="anonymous"></script> <!-- Importa os ícones -->
 
